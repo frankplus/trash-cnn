d432cf71c26e0710382c0f634c21894983f1ded2
Francesco Pham <pham.francesco@gmail.com>
2019-04-01 18:43:54 +0200

    Created model

---
+++ model.py
@@ -0,0 +1,32 @@
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout, Activation
+from keras import backend as K
+
+
+
+def generate_model(input_shape, num_classes):
+    # create model
+    model = Sequential()
+    # add model layers
+    model.add(Conv2D(96, kernel_size=11, strides=4, activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D(pool_size=3, strides=2))
+    model.add(Conv2D(256, kernel_size=5, strides=1, activation='relu'))
+    model.add(MaxPooling2D(pool_size=3, strides=2))
+    model.add(Conv2D(384, kernel_size=3, strides=1, activation='relu'))
+    model.add(Conv2D(384, kernel_size=3, strides=1, activation='relu'))
+    model.add(Conv2D(256, kernel_size=3, strides=1, activation='relu'))
+    model.add(MaxPooling2D(pool_size=3, strides=2))
+
+    model.add(Flatten())
+    model.add(Dropout(0.5))
+    model.add(Dense(4096))
+    model.add(Activation(lambda x: K.relu(x, alpha=1e-6)))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096))
+    model.add(Activation(lambda x: K.relu(x, alpha=1e-6)))
+    model.add(Dense(num_classes, activation="softmax"))
+
+    # compile model using accuracy to measure model performance
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
--- train.py
+++ train.py
@@ -1,16 +1,13 @@
-from keras.datasets import mnist
-from keras.utils import to_categorical
-import matplotlib.pyplot as plt
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.preprocessing.image import ImageDataGenerator
 from keras import backend as K
+from model import generate_model
 
 #parameters
-img_width, img_height = 512, 384 # dimensions of our images.
+img_width, img_height = 128, 128 # dimensions to which the images will be resized
 dataset_dir = 'dataset-resized'
-epochs = 1
+epochs = 10
 batch_size = 4
+num_classes = 6 #categories of trash
 
 # this is the augmentation configuration we will use for training
 dataset_generator = ImageDataGenerator(
@@ -38,16 +35,7 @@ else:
     input_shape = (img_width, img_height, 3)
 
 
-#create model
-model = Sequential()
-#add model layers
-model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=input_shape))
-model.add(Conv2D(32, kernel_size=3, activation='relu'))
-model.add(Flatten())
-model.add(Dense(6, activation='softmax'))
-
-#compile model using accuracy to measure model performance
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model = generate_model(input_shape, num_classes)
 
 
 model.fit_generator(
